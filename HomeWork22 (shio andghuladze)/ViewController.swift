84b841131424c9f2fcc621e87af6f73e9a861beb
shio andghuladze <thegwfg@gmail.com>
2022-08-13 12:12:14 +0400

    Initial Commit

---
+++ HomeWork22 (shio andghuladze)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork22 (shio andghuladze)
+//
+//  Created by shio andghuladze on 13.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
